4383013893b6059359d2681b610872309841cc2f
Saurabh <sauravvishwakarma030@gmail.com>
2025-02-10 18:38:13 +0530

    Added central boto3 object setup

---
+++ server/core/boto3_client.py
@@ -0,0 +1,9 @@
+import boto3
+from config import settings
+
+
+s3_client = boto3.client(
+    "s3",
+    aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+)
