2b53224b610337e69b80a73c677251e71f3196c8
Saurabh <sauravvishwakarma030@gmail.com>
2025-02-10 18:38:39 +0530

    Added added migration to create blurhash field in blurhash table

---
+++ server/scripts/migrations/versions/463fb820ea97_create_metadata_column_in_images_table.py
@@ -0,0 +1,26 @@
+"""create metadata column in images table
+
+Revision ID: 463fb820ea97
+Revises: 81e7346f8f87
+Create Date: 2025-02-10 16:00:02.035515
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects.postgresql import JSONB
+
+# revision identifiers, used by Alembic.
+revision: str = '463fb820ea97'
+down_revision: Union[str, None] = '81e7346f8f87'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.add_column('images', sa.Column('_metadata', JSONB(), nullable=True))
+
+
+def downgrade() -> None:
+    op.drop_column('images', '_metadata')
