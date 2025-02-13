44fc8c5a68810c596d61e91de2bfc1efa28d78da
Saurabh <sauravvishwakarma030@gmail.com>
2025-02-13 22:48:19 +0530

    lint: fixed linting

--- webapp/src/components/ImageWithBlurhash.tsx
+++ webapp/src/components/ImageWithBlurhash.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Image } from "../types";
 import { Blurhash } from "react-blurhash";
 interface ImageWithBlurhashParm {
