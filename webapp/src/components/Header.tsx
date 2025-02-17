7642d4f8140754a752e94c01d1cb8fe905b37bc0
Saurabh <sauravvishwakarma030@gmail.com>
2025-02-17 22:05:04 +0530

    feat: added logout dialog

--- webapp/src/components/Header.tsx
+++ webapp/src/components/Header.tsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router";
+import { removeTokenFromStorage } from "../service/TokenManager";
 
 const toggleUploadContentModel = () => {
   const modal = document.getElementById("my_modal_1");
@@ -8,11 +9,6 @@ const toggleUploadContentModel = () => {
 };
 
 const Header = () => {
-  const navigator = useNavigate();
-  const handle_logout = () => {
-    navigator("/login");
-  };
-
   return (
     <div className="bg-gray-800 text-white px-4 py-3">
       <div className="container mx-auto flex items-center justify-between">
@@ -77,17 +73,67 @@ const Header = () => {
           </div>
 
           <div className="md:flex hidden">
+            <LogoutDialog />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const LogoutDialog = () => {
+  const navigator = useNavigate();
+  const handle_logout = () => {
+    removeTokenFromStorage();
+    navigator("/login");
+  };
+  return (
+    <>
+      {/* Open the modal using document.getElementById('ID').showModal() method */}
+      <button
+        className="btn btn-primary flex items-center justify-center"
+        onClick={() => document.getElementById("logout_model").showModal()}
+      >
+        <span className="ri-logout-circle-line"></span>
+        Logout
+      </button>
+      <dialog
+        id="logout_model"
+        className="modal modal-bottom sm:modal-middle  w-full"
+      >
+        <div className="modal-box flex flex-col w-fit items-center py-12">
+          <div className="h-16 w-16 bg-red-100 rounded-full outline-8 outline-red-100 mb-4">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="red"
+              width="full"
+              height="full"
+            >
+              <path d="M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22ZM12 20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4C7.58172 4 4 7.58172 4 12C4 16.4183 7.58172 20 12 20ZM11 15H13V17H11V15ZM11 7H13V13H11V7Z"></path>
+            </svg>
+          </div>
+          <p className=" text-black pb-8 mt-4">
+            Are you sure you want to log out?
+          </p>
+          <div className="flex items-center justify-center p-0 gap-4">
+            <div className="modal-action mt-0 ml-auto">
+              <form method="dialog">
+                {/* if there is a button in form, it will close the modal */}
+                <button className="btn btn-primary">Cancel</button>
+              </form>
+            </div>
             <button
               type="submit"
-              className="btn btn-primary"
               onClick={handle_logout}
+              className="btn bg-red-500 text-white"
             >
               Logout
             </button>
           </div>
         </div>
-      </div>
-    </div>
+      </dialog>
+    </>
   );
 };
 
