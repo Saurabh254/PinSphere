57e52d5320675524c017d3a847c37f113d675c5c
Saurabh <75117608+Saurabh254@users.noreply.github.com>
2025-04-22 18:45:05 +0530

    Added prompt Versioning (v1) (#42)
    * feat: Added prompt versioning v1
    
    * type and lint: fix type checking and linting

--- server/core/embedding_generation.py
+++ server/core/embedding_generation.py
@@ -7,6 +7,7 @@ from sentence_transformers import SentenceTransformer
 
 from config import settings
 
+from .prompts import PROMPT_V1
 from .storage import get_image
 
 
@@ -20,11 +21,12 @@ def convert_image_to_text(key: str) -> str:
     response = ollama.chat(  # type: ignore
         model="gemma3:4b",
         messages=[
+            {"role": "system", "content": PROMPT_V1.SYSTEM_PROMPT},
             {
                 "role": "user",
-                "content": "What is in this image? in max 20 words. in simple words so that I can implement vector search on this texts and strictly speaking only reply with the resulting sentence nothing else. ",
+                "content": PROMPT_V1.USER_PROMPT,
                 "images": [img_bytes],
-            }
+            },
         ],
     )
     logging.info("Generated the text: {}".format(response))
