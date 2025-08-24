a1079857a2c7a07434a6bb76147abf9318ba970c
Saurabh <75117608+Saurabh254@users.noreply.github.com>
2025-08-24 16:15:53 +0530

    feat: fix typing issue (#53)

--- webapp/src/utils.ts
+++ webapp/src/utils.ts
@@ -1,4 +1,5 @@
-export function getRandomInteger(min, max) {
+
+export function getRandomInteger(min: number, max: number): number {
     min = Math.ceil(min); // Ensure min is an integer
     max = Math.floor(max); // Ensure max is an integer
     return Math.floor(Math.random() * (max - min + 1)) + min;
